46227a4ed10b53ad85125c89cecd58fddd2a653c
Andrew Gazelka <andrew.gazelka@gmail.com>
2023-04-29 14:36:08 -0500

    add ability for custom sys message

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use std::pin::pin;
 use std::process::ExitCode;
 
 use anyhow::{ensure, Context};
+use clap::Parser;
 use futures::future::Either;
 use futures::StreamExt;
 use once_cell::sync::Lazy;
 use tokio::io::{stdin, AsyncReadExt};
+use tokio_openai::ChatRequest;
 use tokio_util::sync::CancellationToken;
 
 static CANCEL_TOKEN: Lazy<CancellationToken> = Lazy::new(CancellationToken::new);
@@ -25,7 +27,27 @@ async fn main() -> ExitCode {
     ExitCode::SUCCESS
 }
 
+#[derive(Parser)]
+#[clap(author, about, version)]
+struct Args {
+    /// System message as a file
+    #[clap(short, long)]
+    sys: Option<String>,
+}
+
+async fn sys_message(path: &str) -> anyhow::Result<String> {
+    let res = tokio::fs::read_to_string(path).await?;
+    Ok(res)
+}
+
 async fn run() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let sys_message = match args.sys {
+        Some(path) => Some(sys_message(&path).await?),
+        None => None,
+    };
+
     // read all stdin
     let mut input = Vec::new();
     let openai = tokio_openai::Client::simple()?;
@@ -34,13 +56,15 @@ async fn run() -> anyhow::Result<()> {
     ensure!(!input.is_empty(), "no input from stdin");
     let input = String::from_utf8(input).context("invalid utf8")?;
 
-    let instructions = std::env::args().skip(1).collect::<Vec<_>>();
-    ensure!(!instructions.is_empty(), "no instructions provided");
+    let mut request = ChatRequest::new();
 
-    let instructions = instructions.join(" ");
+    if let Some(sys_message) = sys_message {
+        request = request.sys_msg(sys_message);
+    }
 
-    let input = format!("{input}\n---\n{instructions}");
-    let mut res = openai.stream_chat(&input).await?;
+    request = request.user_msg(input);
+
+    let mut res = openai.stream_chat(request).await?;
 
     loop {
         let next = res.next();
